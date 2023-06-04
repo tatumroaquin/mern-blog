906483e6a0ed044c259c1c324ac8f1798aa83946
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-06-04 13:35:19 +1000

    feat: use dotenv, mongoose, morgan, and routes'
    - use dotenv to store environment variables.
    - use morgan to log requests to the API end-point.
    - use mongoose to connect with the mernBlogDB

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from './middleware/cors.ts';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+
 import userRoutes from './routes/user.routes.ts';
+import authRoutes from './routes/auth.routes.ts';
+
+dotenv.config({ path: '../.env' });
 
 const app = express();
 
@@ -8,7 +15,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(morgan('tiny'));
+
+mongoose.connect(process.env.MONGODB_URI!).then(() => {
+  console.log('[mongodb]: connection successful');
+}).catch((err) => console.log(`[mongodb]: connection failed! ${err.message}`));
+
 app.use(userRoutes);
+app.use(authRoutes);
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log('[express]: listening on port 8000');
+});
