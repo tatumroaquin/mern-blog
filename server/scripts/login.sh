e42b642299b4f7222280dbcbeb1f1665f28e8ac7
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-06-08 21:01:21 +1000

    chore: add more scripts to refresh, signup/signin

--- server/scripts/login.sh
+++ server/scripts/login.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 curl \
+  -v \
 	-H 'Content-Type: application/json' \
-	-d "{\"email\":\"$1\", \"password\":\"$2\"}" \
+	-d "{\"email\":\"$1\", \"password\":\"Passw0rd12345\"}" \
 	'http://localhost:8000/signin'
---
+++ server/scripts/refresh.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+curl \
+	-G \
+	-b 'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODExZTBkMDBlMGIzNzVmYmRjNDYxMCIsInJvbGVzIjpbInVzZXIiXSwiaWF0IjoxNjg2MjA0MzkxLCJleHAiOjE2ODYyOTA3OTF9.eQVGpj7Y8NBrHfTf4fUYEAF9yEXBVUG0H3_4ZWLYwE8; Max-Age=86400; Path=/; Expires=Fri, 09 Jun 2023 06:06:31 GMT; HttpOnly' \
+	'http://localhost:8000/refresh'
