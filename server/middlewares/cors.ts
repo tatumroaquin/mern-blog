072b15c39a136104ad7ccc6c0570db9742977fcd
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-06-08 19:16:00 +1000

    fix(typo): ACL Origin spelled incorrectly

--- server/middlewares/cors.ts
+++ server/middlewares/cors.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export default function (origin?: string) {
   return function (_: Request, res: Response, next: NextFunction) {
     const domain = origin ?? process.env.CLIENT_URL;
-    res.setHeader('Acess-Control-Allow-Origin', domain ?? '*');
+    res.setHeader('Access-Control-Allow-Origin', domain ?? '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
       'Origin X-Requested-With Accept, Content-Type, Authorization'
