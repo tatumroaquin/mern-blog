befae14fe90a52ca33dfec2beb7ce102da1d43a0
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-06-14 23:00:05 +1000

    feat: initial implemention of page component

---
+++ client/src/pages/Post.tsx
@@ -0,0 +1,49 @@
+import { FC } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import oneDark from 'react-syntax-highlighter/dist/esm/styles/prism/one-dark';
+
+import styles from './Post.module.scss';
+
+interface Post {
+  children: string;
+}
+
+export const Post: FC<Post> = ({ children }) => {
+  return (
+    <article className={styles['post']}>
+      <h1 className={styles['title']}>How to Title</h1>
+      <div className={styles['date']}>
+        <small>
+          Created <b>June 14, 2023</b> | Updated <b>June 23, 2023</b>
+        </small>
+        <small>
+          Posted by <b>Tatum Roaquin</b>
+        </small>
+      </div>
+
+      <ReactMarkdown
+        children={children}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={oneDark}
+                language={match[1]}
+                PreTag='div'
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </article>
+  );
+};
