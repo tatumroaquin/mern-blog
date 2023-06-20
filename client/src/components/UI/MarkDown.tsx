39a06507736199d4e6dc2e08f6739bb6b18adcaf
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-06-20 14:27:09 +1000

    feat: add markdown ui component

---
+++ client/src/components/UI/MarkDown.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import clsx from 'clsx';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import oneDark from 'react-syntax-highlighter/dist/esm/styles/prism/one-dark';
+
+interface Markdown {
+  children: string;
+  className?: string;
+}
+
+export const MarkDown: FC<Markdown> = ({ children, className }) => {
+  return (
+    <article className={clsx({ [`${className}`]: !!className })}>
+      <ReactMarkdown
+        children={String(children)}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={oneDark}
+                language={match[1]}
+                PreTag='div'
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
+    </article>
+  );
+};
