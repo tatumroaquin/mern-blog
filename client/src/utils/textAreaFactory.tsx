8594e26949c63500776f5197e28d3f33b23b9924
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-06-20 14:37:43 +1000

    feat: add textarea object factory

---
+++ client/src/utils/textAreaFactory.tsx
@@ -0,0 +1,39 @@
+import { ITextAreaFactory, ITextAreaObject, IRenderTextArea } from '../types';
+
+import { TextArea } from '../components/UI/TextArea';
+
+export const textAreaFactory = ({
+  name,
+  value,
+  defaultValue = '',
+  placeholder = '',
+}: ITextAreaFactory): ITextAreaObject => {
+  return {
+    name,
+    value: value || defaultValue,
+    valid: false,
+    errorMessage: '',
+    placeholder,
+    touched: false,
+    renderInput: ({
+      key,
+      value,
+      placeholder,
+      errorMessage,
+      isValid,
+      onChange,
+    }: IRenderTextArea) => {
+      return (
+        <TextArea
+          key={key}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          isValid={isValid}
+          errorMessage={errorMessage}
+          onChange={onChange}
+        />
+      );
+    },
+  };
+};
