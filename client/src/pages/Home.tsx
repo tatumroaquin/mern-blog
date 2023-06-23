e21ebe4e76397d535bfcb154dce4e1a5987e5dc0
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-06-23 11:04:51 +1000

    feat: `useHttp` hook to render recent post

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,24 +1,66 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from '../components/UI/Card';
+import { Spinner } from '../components/UI/Spinner';
+import { useHttp } from '../hooks/useHttp';
+import { useEffect } from 'react';
+
 import styles from './Home.module.scss';
 
 export const Home = () => {
+  const [posts, setPosts] = useState<any>([]);
+  const { isLoading, error, setError, sendRequest } = useHttp();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const fetchPosts = async () => {
+      try {
+        const response = await sendRequest({
+          url: `${import.meta.env.VITE_SERVER_URL}/post/all?page=1&limit=5`,
+          abortController,
+        });
+        if (response) {
+          setPosts(response.result.data);
+          console.log(response.result.data);
+        }
+      } catch (e: any) {
+        console.log(e);
+      }
+    };
+    fetchPosts();
+  }, [sendRequest]);
+
   return (
     <article className={styles['home']}>
       <h1 className={styles['home__title']}>Recent Posts</h1>
-      <div className={styles['posts']}>
-        {[1, 2, 3].map(() => (
-          <Card className={styles['post__card']}>
-            <h2 className={styles['post__card--title']}>How to Title a card</h2>
-            <small className={styles['post__card--description']}>
-              Lorem ipsum dolor sit amet, qui minim labore adipisicing minim
-              sint cillum sint consectetur cupidatat.
-            </small>
-            <small className={styles['post__card--date']}>
-              Created <b>July 15, 2023</b> by <b>Tatum Roaquin</b>
-            </small>
-          </Card>
-        ))}
-      </div>
+      <br />
+      {isLoading && <Spinner />}
+      {!isLoading && posts && (
+        <div className={styles['posts']}>
+          {posts.map((post: any) => (
+            <Card className={styles['post__card']} key={post._id}>
+              <h2 className={styles['post__card--title']}>
+                <Link to={`/post/view/${post.slug}`}>{post.title}</Link>
+              </h2>
+              {post.description && (
+                <small className={styles['post__card--description']}>
+                  {post.description}
+                </small>
+              )}
+              <small className={styles['post__card--date']}>
+                Created{' '}
+                <b> {new Date(post.createdAt).toLocaleDateString('en-AU')} </b>{' '}
+                by{' '}
+                <b>
+                  <Link to={`/user/view/${post.author._id}`}>
+                    {post.author.firstName} {post.author.lastName}
+                  </Link>
+                </b>
+              </small>
+            </Card>
+          ))}
+        </div>
+      )}
     </article>
   );
 };
