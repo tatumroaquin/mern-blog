98a0fc9f18f95030bd03c10665a538d10c42cf21
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-07-19 13:08:22 +1000

    feat: add security checks when getting user by id
    - verify RT details in cookie matches user's get request
    - disallow any attempt to view other people's accounts.
    - implement initial logic to edit user account details.

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,11 +1,77 @@
 import { Request, Response } from 'express';
+import JWT, { JwtPayload } from 'jsonwebtoken';
 import User from '../models/User.model.ts';
+import Token from '../models/Token.model.ts';
 
-export async function getUserById(req: Request, res: Response) {
+interface IUserRequest extends Request {
+  accessTokenPayload?: JwtPayload | { id: string };
+  refreshTokenPayload?: JwtPayload | { id: string };
+}
+
+export async function getUserByIdController(req: IUserRequest, res: Response) {
+  const userId = req.params.id;
+  const RTPayload = req.refreshTokenPayload;
+
+  let token;
+  try {
+    token = await Token.findOne({ userId });
+  } catch (e: any) {
+    return res.status(403).json({ error: e.message });
+  }
+
+  if (!token?.userId.equals(RTPayload?.id))
+    return res
+      .status(403)
+      .json({ error: "You cannot view other people's accounts" });
+
   const user = await User.findOne(
-    { _id: req.params.id },
-    { email: false, passwordHash: false, roles: false } 
+    { _id: userId },
+    { email: false, passwordHash: false }
   );
+
   if (!user) return res.sendStatus(404);
   res.json(user);
+}
+
+export async function editUserController(req: Request, res: Response) {
+  const {
+    userId,
+    userName,
+    firstName,
+    lastName,
+    oldPassword,
+    newPassword,
+    accessToken,
+  } = req.body;
+
+  let payload: any;
+  try {
+    payload = JWT.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET!);
+  } catch (e: any) {
+    return res.json({ error: 'Invalid access token provided' });
+  }
+
+  if (payload.id !== userId)
+    return res.json({ error: 'User Id does not match token' });
+
+  const user = await User.findOne({ _id: payload.id });
+
+  if (!user) return res.json({ error: 'User does not exist' });
+
+  if (!user.authenticate(oldPassword))
+    return res.json({
+      error: 'Your current password does not match our records',
+    });
+
+  try {
+    user.userName = userName;
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.password = newPassword;
+    await user.save();
+  } catch (e: any) {
+    return res.json({ error: e.message });
+  }
+
+  res.json({ success: 'User details has been updated' });
 }
