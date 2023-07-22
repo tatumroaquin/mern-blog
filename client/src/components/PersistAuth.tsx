c8b5da4954973a6567343db505e2d44b72d1a677
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-07-22 10:31:28 +1000

    feat: implement persistent auth component

---
+++ client/src/components/PersistAuth.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useTokenRefresh } from '../hooks/useTokenRefresh';
+import { useAuth } from '../hooks/useAuth';
+import { Spinner } from './UI/Spinner';
+
+export const PersistAuth = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { auth, persist } = useAuth();
+  const refresh = useTokenRefresh();
+
+  useEffect(() => {
+    let isMounted = true;
+    const refreshToken = async () => {
+      try {
+        await refresh();
+      } catch (e: any) {
+        console.log(e.message);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
+    const hasAccessToken = Boolean(auth?.accessToken);
+
+    if (!hasAccessToken && persist) {
+      refreshToken();
+    } else {
+      setIsLoading(false);
+    }
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`);
+    console.log(`authContext: ${JSON.stringify(auth)}`);
+  }, [isLoading]);
+
+  return (
+    <>
+      {!persist && <Outlet />}
+      {persist && isLoading && <Spinner />}
+      {persist && !isLoading && <Outlet />}
+    </>
+  );
+};
