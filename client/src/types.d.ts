5b2c4cf0a8f0cd7a575c7201cec05eb1733f450a
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-07-23 16:50:03 +1000

    refactor: condense typedefs using abstraction

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,44 +1,93 @@
-export interface IInput {
-  name: string;
-  type: string;
-  label: string;
-  placeholder?: string;
-  value?: string;
+export interface IInputElement {
+  className?: string;
+  defaultValue?: string;
   errorMessage?: string;
   isValid: boolean;
+  name: string;
+  placeholder?: string;
+  type?: string;
+  value?: string | any[];
+  label?: string;
+}
+
+
+export interface IInput extends IInputElement {
   onChange: (e: FormEvent<HTMLInputElement>) => void;
 }
 
-export interface IRenderInput {
-  key: string;
-  value?: string;
+export interface ITextArea extends IInputElement {
+  onChange: (e: FormEvent<HTMLInputElement>) => void;
+}
+
+export interface ISelect {
+  label: string;
+  name: string;
   placeholder?: string;
   errorMessage: string;
   isValid: boolean;
-  onChange: (e: FormEvent<HTMLInputElement>) => void;
+  onChange: (newValue: MultiValue<typeof Option>) => void;
 }
 
-export interface IInputFactory {
-  label: string;
+export interface IRenderElement {
+  key: string;
+  value?: string | any[];
+  placeholder?: string;
+  errorMessage: string;
+  isValid: boolean;
+}
+
+export interface IRenderInput  extends IRenderElement {
+  onChange: (e: FormEvent<HTMLInputElement>) => void;
+  value?: string | string[];
+}
+
+export interface IRenderTextArea extends IRenderElement {
+  onChange: (e: FormEvent<HTMLTextAreaElement>) => void;
+}
+
+export interface IRenderSelect extends IRenderElement {
+  onChange: (items: MultiValue<typeof Option>) => void;
+}
+
+export interface IFactoryElement {
+  type?: string;
   name: string;
-  type: string;
+  value?: string | string[];
   defaultValue?: string;
   placeholder?: string;
+}
+
+
+export interface IInputFactory extends IFactoryElement {
+  label: string;
+}
+
+export interface ITextAreaFactory extends IFactoryElement {
+  defaultValue?: string;
+}
+
+interface ISelectFactory extends IFactoryElement {
+  label: string;
 }
 
 export interface IValidationRule {
   name: string;
   errorMessage: string;
-  validate: (inputValue: string, formObject?: IFormObject) => boolean;
+  validate: (inputValue: string | any[], formObject: IFormObject) => boolean;
 }
 
-export interface IInputObject {
+export interface IFormObjectElement {
+  type?: string;
   name: string;
-  value: string;
+  value: string | string[];
   valid: boolean;
   errorMessage: string;
   touched: boolean;
   placeholder?: string;
+  validationRules?: IValidationRule[]
+}
+
+export interface IInputObject extends IFormObjectElement {
   renderInput: ({
     key,
     value,
@@ -47,36 +96,9 @@ export interface IInputObject {
     isValid,
     onChange,
   }: IRenderInput) => JSX.Element;
-  validationRules?: IValidationRule[]
 }
 
-export interface ITextArea {
-  name: string;
-  placeholder?: string;
-  value?: string;
-  errorMessage?: string;
-  className?: string;
-  defaultValue?: string;
-  isValid: boolean;
-  onChange: (e: FormEvent<HTMLInputElement>) => void;
-}
-
-export interface IRenderTextArea {
-  key: string;
-  value?: string;
-  placeholder?: string;
-  errorMessage: string;
-  isValid: boolean;
-  onChange: (e: FormEvent<HTMLTextAreaElement>) => void;
-}
-
-export interface ITextAreaObject {
-  name: string;
-  value: string;
-  valid: boolean;
-  errorMessage: string;
-  touched: boolean;
-  placeholder?: string;
+export interface ITextAreaObject extends IFormObjectElement {
   renderInput: ({
     key,
     value,
@@ -85,17 +107,18 @@ export interface ITextAreaObject {
     isValid,
     onChange,
   }: IRenderTextArea) => JSX.Element;
-  validationRules?: IValidationRule[]
 }
 
-export interface ITextAreaFactory {
-  name: string;
-  value?: string;
-  defaultValue?: string;
-  placeholder?: string;
+export interface ISelectObject extends IFormObjectElement {
+  renderInput: ({
+    key,
+    placeholder,
+    errorMessage,
+    isValid,
+    onChange,
+  }: IRenderSelect) => JSX.Element;
 }
-
 
 export interface IFormObject {
-  [field: string]: IInputObject | ITextAreaObject;
+  [field: string]: IInputObject | ITextAreaObject | ISelectObject;
 }
