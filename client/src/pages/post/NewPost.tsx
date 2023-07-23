646b919341aa59ab1ba4be7dcf78137aa4560c5b
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-07-23 16:45:17 +1000

    feat: implement new posts submission logic
    - add spinner to render loading state of `useHttpPrivate`
    - `useHttpPrivate` to submit requests with `jwt` cookie
    - make `onSubmit` async, handle clean up of markdown text
    - use `markdown` to get first line as title and trim spaces.
    - use `markdown` to convert post tags to array of strings.
    - use `isLoading` state to render spinner and the forms.

--- client/src/pages/post/NewPost.tsx
+++ client/src/pages/post/NewPost.tsx
@@ -1,20 +1,59 @@
 import { FormEvent } from 'react';
 import { MarkDown } from '../../components/UI/MarkDown';
 import { Button } from '../../components/UI/Button';
+import { Spinner } from '../../components/UI/Spinner';
 import { postForm } from '../../components/Form/PostForm';
 import { useForm } from '../../hooks/useForm';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useHttpPrivate } from '../../hooks/useHttpPrivate';
 
 import styles from './NewPost.module.scss';
 
 export const NewPost = () => {
   const [markdown, setMarkdown] = useLocalStorage('markdown');
+  const { isLoading, sendRequest } = useHttpPrivate();
 
   postForm.markdown.value = markdown;
   const { renderForm } = useForm(postForm);
 
-  function onSubmit(e: FormEvent) {
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    let { markdown, description, postTags } = e.target as HTMLFormElement;
+
+    const lines = markdown.value.split('\n');
+
+    const firstLine = lines.find((str: string) => str.length !== 0);
+
+    const title = firstLine.replace('#', '').trim();
+
+    markdown = lines
+      .filter((line: string) => line !== firstLine)
+      .join('\n')
+      .trim();
+
+    description = description.value;
+    if (postTags instanceof RadioNodeList)
+      postTags = Object.values(postTags).map((tag: any) => tag.value);
+    else if (typeof postTags.value === 'string' && postTags.value.length !== 0)
+      postTags = [postTags.value];
+    else postTags = [];
+
+    console.log('title', title);
+    console.log('markdown', markdown);
+    console.log('description', description);
+    console.log('tags', postTags);
+
+    const abortController = new AbortController();
+    const response = await sendRequest({
+      url: `${import.meta.env.VITE_SERVER_URL}/post/new`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, description, markdown, tags: postTags }),
+      abortController,
+    });
+    console.log(response);
   }
 
   function onChange(e: FormEvent<HTMLFormElement>) {
@@ -22,20 +61,26 @@ export const NewPost = () => {
     if (name !== 'markdown') return;
     setMarkdown(value);
   }
+
   return (
-    <div className={styles['post-new']}>
-      <form
-        onChange={onChange}
-        onSubmit={onSubmit}
-        className={styles['post-new__form']}
-      >
-        {renderForm()}
-        <Button>Submit</Button>
-      </form>
-      <MarkDown
-        className={styles['post-new__markdown']}
-        children={markdown ?? ''}
-      />
-    </div>
+    <>
+      {isLoading && <Spinner />}
+      {!isLoading && (
+        <div className={styles['post-new']}>
+          <form
+            onChange={onChange}
+            onSubmit={onSubmit}
+            className={styles['post-new__form']}
+          >
+            {renderForm()}
+            <Button>Submit</Button>
+          </form>
+          <MarkDown
+            className={styles['post-new__markdown']}
+            children={markdown ?? ''}
+          />
+        </div>
+      )}
+    </>
   );
 };
