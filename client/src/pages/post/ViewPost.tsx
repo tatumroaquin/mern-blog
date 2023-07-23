64a35e75c634e8d3984ff83308d5152cce116710
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-07-23 16:50:33 +1000

    feat: implement logic to get posts content from db
    - `useHttp` to send requests and handle loading state
    - `useParams` to get the post slug for get request
    - `useState` to store post content to page state
    - `isLoading` to only show post when it is truthy

--- client/src/pages/post/ViewPost.tsx
+++ client/src/pages/post/ViewPost.tsx
@@ -1,49 +1,72 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import oneDark from 'react-syntax-highlighter/dist/esm/styles/prism/one-dark';
 
 import styles from './ViewPost.module.scss';
+import { useHttp } from '../../hooks/useHttp';
+import { useParams } from 'react-router-dom';
 
-interface ViewPost {
-  children: string;
-}
+export const ViewPost: FC = () => {
+  const { isLoading, sendRequest } = useHttp();
+  const { slug } = useParams();
+  const [post, setPost] = useState<any>();
 
-export const ViewPost: FC<ViewPost> = ({ children }) => {
+  useEffect(() => {
+    const fetchPost = async () => {
+      const abortController = new AbortController();
+      const response = await sendRequest({
+        url: `${import.meta.env.VITE_SERVER_URL}/post/view/${slug}`,
+        abortController,
+      });
+      console.log(response);
+      if (response) setPost(response.post);
+    };
+    fetchPost();
+  }, [slug, sendRequest]);
+
   return (
-    <article className={styles['post']}>
-      <h1 className={styles['title']}>How to Title</h1>
-      <div className={styles['date']}>
-        <small>
-          Created <b>June 14, 2023</b> | Updated <b>June 23, 2023</b>
-        </small>
-        <small>
-          Posted by <b>Tatum Roaquin</b>
-        </small>
-      </div>
+    <>
+      {!isLoading && post && (
+        <article className={styles['post']}>
+          <h1 className={styles['title']}>{post.title}</h1>
+          <div className={styles['date']}>
+            <small>
+              Created{' '}
+              <b>{new Date(post.createdAt).toLocaleDateString('en-AU')}</b> |
+              Updated{' '}
+              <b>{new Date(post.updatedAt).toLocaleDateString('en-AU')}</b>
+            </small>
+            <small>
+              Posted by{' '}
+              <b>{`${post.author.firstName} ${post.author.lastName} (${post.author.userName})`}</b>
+            </small>
+          </div>
 
-      <ReactMarkdown
-        children={children}
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter
-                children={String(children).replace(/\n$/, '')}
-                style={oneDark}
-                language={match[1]}
-                PreTag='div'
-              />
-            ) : (
-              <code {...props} className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
-      />
-    </article>
+          <ReactMarkdown
+            children={post.markdown}
+            remarkPlugins={[remarkGfm]}
+            components={{
+              code({ inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '');
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, '')}
+                    style={oneDark}
+                    language={match[1]}
+                    PreTag='div'
+                  />
+                ) : (
+                  <code {...props} className={className}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          />
+        </article>
+      )}
+    </>
   );
 };
