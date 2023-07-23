c9cb338bdd48b9ff87fa0e4505413589cd0e163f
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-07-23 11:16:58 +1000

    feat: add route protect, persist auth, signout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/UI/Layout';
 import { Home } from './pages/Home';
 import { ViewPost } from './pages/post/ViewPost';
@@ -9,32 +7,60 @@ import { SignUp } from './pages/SignUp';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { NewPost } from './pages/post/NewPost';
-import { useLocalStorage } from './hooks/useLocalStorage';
+import { ProtectRoute } from './components/ProtectRoute';
+import { ViewUser } from './pages/ViewUser';
+import { PersistAuth } from './components/PersistAuth';
+import { SignOut } from './pages/SignOut';
 
 function App() {
-  const [markdown, setMarkdown] = useLocalStorage('markdown');
+  const ROLES = {
+    USER: 'user',
+    ADMIN: 'admin',
+  };
 
   return (
-    <>
-        <Router>
-          <Layout>
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path='post'>
-                <Route path='new' element={<NewPost />} />
-                <Route path='view' element={<ViewPost children={markdown}/>} />
-                <Route path='edit' element={<Home />} />
-              </Route>
-              <Route path='auth'>
-                <Route path='signin' element={<SignIn />} />
-                <Route path='signup' element={<SignUp />} />
-              </Route>
-              <Route path='about' element={<About />} />
-              <Route path='contact' element={<Contact />} />
-            </Routes>
-          </Layout>
-        </Router>
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route element={<PersistAuth />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+
+          <Route path='post'>
+            <Route path='view'>
+              <Route path=':slug' element={<ViewPost />} />
+            </Route>
+          </Route>
+
+          <Route path='auth'>
+            <Route path='signin' element={<SignIn />} />
+            <Route path='logout' element={<SignOut />} />
+          </Route>
+
+          <Route
+            path='post'
+            element={<ProtectRoute allowedRoles={[ROLES.USER]} />}
+          >
+            <Route path='new' element={<NewPost />} />
+            <Route path='edit' element={<Home />} />
+          </Route>
+
+          <Route
+            path='auth'
+            element={<ProtectRoute allowedRoles={[ROLES.ADMIN]} />}
+          >
+            <Route path='signup' element={<SignUp />} />
+          </Route>
+
+          <Route
+            path='user'
+            element={<ProtectRoute allowedRoles={[ROLES.ADMIN, ROLES.USER]} />}
+          >
+            <Route path='view' element={<ViewUser />} />
+          </Route>
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
