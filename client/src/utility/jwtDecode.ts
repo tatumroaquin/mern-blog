63a795a7a6d48fb82c6bf97ed8d30983ca45d31b
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-09-01 22:15:54 +1000

    chore(jwtDecode): Add check for empty string

--- client/src/utility/jwtDecode.ts
+++ client/src/utility/jwtDecode.ts
@@ -1,4 +1,5 @@
 export function jwtDecode(jwt: string) {
+  if (!jwt) return '';
   const base64Url = jwt.split('.')[1];
   const base64 = base64Url.replace(/-/g, '+').replace(/_/, '/');
   return JSON.parse(atob(base64));
