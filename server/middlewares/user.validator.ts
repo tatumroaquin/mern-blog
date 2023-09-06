1c05121bf19e13775f351f095b79fd47ff5bad91
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-09-07 08:55:20 +1000

    feat: Add validator for user account update

--- server/middlewares/user.validator.ts
+++ server/middlewares/user.validator.ts
@@ -19,3 +19,18 @@ export const userSignInValidator = [
   check('email').isEmail().withMessage('No user exists with that email'),
   check('password').notEmpty().withMessage('Please enter your password')
 ];
+
+export const editUserValidator = [
+  check('firstName').notEmpty().withMessage('First name must be a valid name'),
+  check('lastName').notEmpty().withMessage('Last name must be a valid name'),
+  check('userName')
+    .isLength({ min: 5 })
+    .matches(/^[a-zA-Z0-9\-_.]+$/g)
+    .withMessage(
+      'Usernames must be 5 characters minimum, and consist of letters, numbers, dashes, underscores, or periods'
+    ),
+  check('email').isEmail().withMessage('Invalid email address'),
+  check('oldPassword')
+    .notEmpty()
+    .withMessage('Password needs to be 10 characters or longer'),
+]
