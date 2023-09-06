1cf9623bdf77ad8b882c6eac720ea54cec50cfd4
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-09-07 08:26:07 +1000

    feat: Attach user update form in view user route

--- client/src/pages/ViewUser.tsx
+++ client/src/pages/ViewUser.tsx
@@ -1,12 +1,17 @@
-import { FC, useState, useEffect } from 'react';
+import { FC, FormEvent, useEffect } from 'react';
 import { useHttpPrivate } from '../hooks/useHttpPrivate';
 import { useAuth } from '../hooks/useAuth';
 import { jwtDecode } from '../utility/jwtDecode';
 
+import { Spinner } from '../components/UI/Spinner';
+import { Button } from '../components/UI/Button';
+import styles from './ViewUser.module.scss';
+import { useForm } from '../hooks/useForm';
+import { accountForm } from '../components/Form/AccountForm';
+
 export const ViewUser: FC = () => {
-  const { sendRequest } = useHttpPrivate();
+  const { isLoading, sendRequest } = useHttpPrivate();
   const { auth } = useAuth();
-  const [userData, setUserData] = useState<any>();
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -15,23 +20,43 @@ export const ViewUser: FC = () => {
       return;
     }
 
-    const accessTokenDetails = jwtDecode(auth.accessToken);
-    const { id } = accessTokenDetails;
-    console.log('AT Details', accessTokenDetails);
+    const user = jwtDecode(auth.accessToken);
+    const { id } = user;
+    console.log('AT Details', user);
 
     const fetchUser = async () => {
       const data = await sendRequest({
         url: `${import.meta.env.VITE_SERVER_URL}/user/${id}`,
         abortController,
       });
-      setUserData(data);
+      accountForm.firstName.value = data.firstName;
+      accountForm.lastName.value = data.firstName;
+      accountForm.username.value = data.userName;
+      accountForm.email.value = data.email;
+      console.log(data);
     };
     fetchUser();
-  }, [sendRequest]);
+  }, [auth?.accessToken, sendRequest]);
+
+  const { renderForm } = useForm(accountForm);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
 
   return (
     <>
-      <code>{JSON.stringify(userData)}</code>
+      <article className={styles['user-form']}>
+        <h1 className={styles['user-form__title']}>Account Details</h1>
+        <br />
+        {isLoading && <Spinner />}
+        {!isLoading && (
+          <form className={styles['user-form__form']} onSubmit={handleSubmit}>
+            {renderForm()}
+            <Button className={styles['user-form__button']}>Update</Button>
+          </form>
+        )}
+      </article>
     </>
   );
 };
