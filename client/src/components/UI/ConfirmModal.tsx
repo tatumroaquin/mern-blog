8bf66ec0be1898b242271a657e1e90501440fe44
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-09-21 20:43:03 +1000

    feat: Implement confirmation modal for yes or no
    - use basic modal UI component to implement a confirmation dialog
    - `onCancel` closes toggles the `show` state to close the modal
    - `onConfirm` calls a function in the lifted state of the parent

---
+++ client/src/components/UI/ConfirmModal.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { Modal } from './Modal';
+import { Button } from './Button';
+
+interface ConfirmModal {
+  show: boolean;
+  header: string;
+  message: string;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+export const ConfirmModal: FC<ConfirmModal> = ({
+  show,
+  header,
+  message = '',
+  onCancel,
+  onConfirm,
+}) => {
+  return (
+    <Modal
+      show={show}
+      header={header}
+      footer={
+        <>
+          <Button onClick={onConfirm}>Yes</Button>
+          <Button onClick={onCancel}>No</Button>
+        </>
+      }
+      onCancel={onCancel}
+    >
+      {message}
+    </Modal>
+  );
+};
