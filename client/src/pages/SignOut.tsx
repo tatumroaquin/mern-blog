e22c2e1e6e802e3dc5f9ca80197afdec8e3d97d5
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-10-06 12:01:44 +1100

    feat(signup): Add alert and error modals for feedback

--- client/src/pages/SignOut.tsx
+++ client/src/pages/SignOut.tsx
@@ -1,6 +1,9 @@
 import { FC, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Spinner } from '../components/UI/Spinner';
+import { AlertModal } from '../components/UI/AlertModal';
+import { ErrorModal } from '../components/UI/ErrorModal';
 
 import { useAuth } from '../hooks/useAuth';
 import { useHttpPrivate } from '../hooks/useHttpPrivate';
@@ -9,27 +12,50 @@ import { useLocalStorage } from '../hooks/useLocalStorage';
 export const SignOut: FC = () => {
   const { auth, setAuth } = useAuth();
   const [_, setMarkdown] = useLocalStorage('markdown');
-  const { isLoading, sendRequest } = useHttpPrivate();
+  const { isLoading, sendRequest, error, setError } = useHttpPrivate();
   const [username] = useState(auth?.username);
+
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     const logout = async () => {
       const abortController = new AbortController();
-      await sendRequest({
+      const response = await sendRequest({
         url: `${import.meta.env.VITE_SERVER_URL}/auth/logout`,
         abortController,
       });
+      if (response.success) {
+        setAlertMessage(response.success);
+        setShowAlert(true);
+      }
     };
     logout();
     setAuth({});
     setMarkdown('');
   }, []);
 
+  function handleOk() {
+    setShowAlert(false);
+    navigate('/', { replace: true });
+  }
+
   return (
     <>
+      <ErrorModal
+        show={!!error}
+        header='Error has occurred!'
+        error={error}
+        onCancel={() => setError('')}
+      />
+      <AlertModal
+        show={showAlert}
+        header='Alert'
+        message={alertMessage}
+        onCancel={handleOk}
+      />
       {isLoading && <Spinner />}
-      {!isLoading && username && <h1>{username} has been logged out.</h1>}
-      {!isLoading && !username && <h1>No authenticated accounts found.</h1>}
     </>
   );
 };
