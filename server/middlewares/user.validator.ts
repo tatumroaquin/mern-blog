a6174a53eed0eef21f85e1902669f7848c7a38e0
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-10-06 11:52:10 +1100

    refactor: Add more user validation criteria.
    - use `normalizeEmail()` to remove string irregularities
    - but allow dots in all major email provider addresses.
    - use `isStrongPassword()` to satisfy minimum security.
    - trim all strings coming to all fields except password.
    - password length is now a maximum of 32 characters.

--- server/middlewares/user.validator.ts
+++ server/middlewares/user.validator.ts
@@ -1,36 +1,124 @@
 import { check } from 'express-validator';
 
 export const userSignUpValidator = [
-  check('firstName').isAlpha().withMessage('First name must be a valid name'),
-  check('lastName').isAlpha().withMessage('Last name must be a valid name'),
+  check('firstName')
+    .trim()
+    .isAlpha()
+    .withMessage('First name must be a valid name'),
+  check('lastName')
+    .trim()
+    .isAlpha()
+    .withMessage('Last name must be a valid name'),
   check('userName')
+    .trim()
     .isLength({ min: 5 })
     .matches(/^[a-zA-Z0-9\-_.]+$/g)
     .withMessage(
       'Usernames must be 5 characters minimum, and consist of letters, numbers, dashes, underscores, or periods'
     ),
-  check('email').isEmail().withMessage('Invalid email address'),
+  check('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Emails must be a valid email')
+    .normalizeEmail({
+      gmail_remove_dots: false,
+      gmail_remove_subaddress: false,
+      yahoo_remove_subaddress: false,
+      icloud_remove_subaddress: false,
+      outlookdotcom_remove_subaddress: false,
+    }),
   check('password')
-    .isLength({ min: 10 })
-    .withMessage('Password needs to be 10 characters or longer'),
+    .notEmpty()
+    .withMessage('Passwords can NOT be empty')
+    .isStrongPassword({
+      minLength: 10,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 2,
+      minSymbols: 1,
+    })
+    .withMessage(
+      'Passwords must be 10-32 characters long, contain: 1 uppercase, 1 lowercase, 1 symbol, and at least 2 numbers.'
+    )
+    .isLength({ max: 32 })
+    .escape(),
 ];
 
 export const userSignInValidator = [
-  check('email').isEmail().withMessage('No user exists with that email'),
-  check('password').notEmpty().withMessage('Please enter your password')
+  check('email')
+    .trim()
+    .isEmail()
+    .withMessage('Emails must be a valid email string')
+    .normalizeEmail({
+      gmail_remove_dots: false,
+      gmail_remove_subaddress: false,
+      yahoo_remove_subaddress: false,
+      icloud_remove_subaddress: false,
+      outlookdotcom_remove_subaddress: false,
+    }),
+  check('password')
+    .notEmpty()
+    .withMessage('Passwords can NOT be empty')
+    .isString()
+    .withMessage('Passwords must be a string')
+    .escape(),
 ];
 
 export const editUserValidator = [
-  check('firstName').notEmpty().withMessage('First name must be a valid name'),
-  check('lastName').notEmpty().withMessage('Last name must be a valid name'),
+  check('firstName')
+    .trim()
+    .notEmpty()
+    .withMessage('First name must NOT be empty')
+    .isAlpha()
+    .withMessage('First name must only contain letters'),
+  check('lastName')
+    .trim()
+    .notEmpty()
+    .withMessage('Last name must NOT be empty')
+    .isAlpha()
+    .withMessage('Last name must only contain letters'),
   check('userName')
+    .trim()
     .isLength({ min: 5 })
     .matches(/^[a-zA-Z0-9\-_.]+$/g)
     .withMessage(
       'Usernames must be 5 characters minimum, and consist of letters, numbers, dashes, underscores, or periods'
     ),
-  check('email').isEmail().withMessage('Invalid email address'),
+  check('email')
+    .trim()
+    .isEmail()
+    .withMessage('Emails must be a valid email string')
+    .normalizeEmail({
+      gmail_remove_dots: false,
+      gmail_remove_subaddress: false,
+      yahoo_remove_subaddress: false,
+      icloud_remove_subaddress: false,
+      outlookdotcom_remove_subaddress: false,
+    })
+    .withMessage('Invalid email address'),
   check('oldPassword')
     .notEmpty()
-    .withMessage('Password needs to be 10 characters or longer'),
-]
+    .withMessage('Enter current password to edit account')
+    .isString()
+    .withMessage('Passwords must be a string')
+    .isLength({ max: 32 })
+    .withMessage('Passwords can not exceed 32 characters')
+    .escape(),
+  check('newPassword')
+    .optional()
+    .isStrongPassword({
+      minLength: 10,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 2,
+      minSymbols: 1,
+    })
+    .withMessage(
+      'Passwords must be 10-32 characters long, contain: 1 uppercase, 1 lowercase, 1 symbol, and at least 2 numbers.'
+    )
+    .isLength({ max: 32 })
+    .withMessage('Passwords can not exceed 32 characters')
+    .escape(),
+];
