445313e9d01e592216fde044a1425219d8f01fed
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-10-06 14:40:48 +1100

    feat: Implement AlertModal component

---
+++ client/src/components/UI/AlertModal.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { Modal } from './Modal';
+import { Button } from './Button';
+
+interface AlertModal {
+  show: boolean;
+  header: string;
+  message: string;
+  onCancel: () => void;
+}
+
+export const AlertModal: FC<AlertModal> = ({
+  show,
+  header,
+  message = '',
+  onCancel,
+}) => {
+  return (
+    <Modal
+      show={show}
+      header={header}
+      footer={
+        <>
+          <Button onClick={onCancel}>Ok</Button>
+        </>
+      }
+      onCancel={onCancel}
+    >
+      {message}
+    </Modal>
+  );
+};
