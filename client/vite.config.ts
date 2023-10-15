e341b64f00d59b16eb5a8330c40f3ea262b39f85
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-10-16 10:23:59 +1100

    build(vite): Use manual chunks, reduce `dist` size
    - get deps from `package.json` and separate them in chunks.
    - disable `sourcemap` to suppress build errors.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { dependencies } from './package.json';
 
-// https://vitejs.dev/config/
+// https://sambitsahoo.com/blog/vite-code-splitting-that-works.html
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, '../') };
   return defineConfig({
+    plugins: [react()],
     server: {
       https: {
         key: '../ssl/tblog-key.pem',
@@ -20,6 +31,16 @@ export default ({ mode }) => {
         },
       },
     },
-    plugins: [react()],
+    build: {
+      sourcemap: false,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom', 'react-router-dom'],
+            ...renderChunks(dependencies),
+          },
+        },
+      },
+    },
   });
 };
