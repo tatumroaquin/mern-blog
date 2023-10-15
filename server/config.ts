94fafe1d053e54417b471e4f08f07be7bc179dd2
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-10-16 10:21:04 +1100

    chore: Add CORS development IP Addresses

--- server/config.ts
+++ server/config.ts
@@ -1,6 +1,12 @@
 export const allowedOrigins = [
   'https://localhost:5173',
   'https://192.168.0.32:5173',
+  'https://192.168.0.33:5173',
+
+  'https://localhost:4173',
+  'https://192.168.0.32:4173',
+  'https://192.168.0.33:4173',
+
 ];
 export const sslCertificates = {
   publicKey: '../ssl/tblog-crt.pem',
