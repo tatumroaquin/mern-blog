c887475f60a0626884c0bf8804dffec99ba5e2ee
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-10-22 16:03:08 +1100

    ci: Try `sudo` workaround change user `ubuntu`

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Install dependencies in server and client
+
+sudo -i -u ubuntu bash << EOF
 pnpm run deps
+EOF
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-DOTENV_KEY="$(aws ssm get-parameter --with-decryption --name '/mern-blog/secrets/DOTENV_KEY' --query "Parameter.Value")"
-export DOTENV_KEY
+# Restart mern-blog app in pm2
 
+sudo -i -u ubuntu bash << EOF
+DOTENV_KEY="$(aws ssm get-parameter --with-decryption --name '/mern-blog/secrets/DOTENV_KEY' --query 'Parameter.Value')"
+export DOTENV_KEY
 pm2 restart mern-blog
+EOF
