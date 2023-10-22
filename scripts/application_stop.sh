bb7727197d4fa7d210550a7a65e0791aee3f0532
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-10-22 21:21:46 +1100

    ci: Remove `DOTENV_KEY` from stop process

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -7,9 +7,6 @@ case ":$PATH:" in
 *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 
-DOTENV_KEY="$(aws ssm get-parameter --with-decryption --name '/mern-blog/secrets/DOTENV_KEY' --query 'Parameter.Value')"
-export DOTENV_KEY
-
 cd "/home/ubuntu/mern-blog" || return
 
 # Stop and delete mern-blog process
